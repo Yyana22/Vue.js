c7085331e60428b07460731fa8ea3852aa872a6b
Yana <sharik.spartak2@mail.ru>
2021-10-25 12:19:45 +0300

    lesson1

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,67 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-html="dataHTML"></div>
+    <HelloWorld msg="Welcome to Your Vue.js App" :date="computedDate"/>
+    <Calculator/>
+
+    <!-- <input @keyup.enter="onEnter"> --> <!-- после нажатия enter выполняется функция onEnter-->
+    <input @keyup="onEnter"> <!-- каждый раз при введении выполняется функция onEnter -->
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" :date="currentDate || new Date()"/> -->
+    <button @click="onClick">Click me </button> / {{count}}
+    <button @click="count = 1 + 2">Sum</button>
+    <button @click="onClick2(1,4)">Click me2</button>
+    {{ message }}
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Calculator,
+  },
+  data(){
+    return {
+      count: 0,
+      currentDate: null,
+      message: "HI",
+      dataHTML: "<div class='data'>123123</div>",
+    }
+  },
+  methods: {
+    onClick(){
+      this.count +=1;
+    },
+    onClick2(param, param2){
+      this.count = param + param2;
+    },
+    onEnter(){
+      console.log('KeyUp');
+    }
+  },
+  computed: {
+    computedDate(){
+      return this.currentDate || new Date();
+    }
+  },
 }
 </script>
 
-<style>
+<style >
+button {
+  background-color: #2c3e50;
+  }
+.data {
+  color: green;
+  background-color: aqua;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Calculator.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <input type="number" v-model.number="operand1" placeholder="operand1">
+        <input type="number" 
+        v-model.number="operand2" 
+        placeholder="operand2" 
+        @blur="sendData(operand2)"/>
+        = {{sum}}
+        <div class="keyboard">
+            <button v-on:click="sum = operand1 + operand2">+</button>
+            <button @click="sum = operand1 - operand2">-</button>
+            <button @click="mult">*</button>
+            <button @click="div(operand1, operand2)">/</button>
+        </div>
+        {{powWithOperand}}
+        {{powSum}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Calculator",
+    data(){
+        return{
+            operand1: 0,
+            operand2: 0,
+            sum: 0,
+        }
+    },
+    //watch позволяет увидеть изменения (новое значение и старое) СМОТРИТ за изменениями
+    watch: {
+        sum: function(newValue, oldValue){
+            // this.sum = this.sum + 1 (будет цикл, т.к. после изменения сумма увеличивается на 1(еще раз меняется))
+            console.log(newValue, oldValue);
+            this.sendData(newValue);
+        }
+    },
+    methods: {
+        mult(){
+            this.sum = this.operand1 * this.operand2;
+        },
+        div(op1, op2){
+            this.sum = op1/op2;
+        },
+        sendData(data){
+            console.log('Send Data name', data);
+        }
+    },
+    //ВЫЧИСЛЯЕТ значения после действий пользователя(?)
+    computed: {
+        powWithOperand(){
+            return Math.pow(this.operand1, this.operand2);
+        },
+        powSum(){
+            return Math.pow(this.sum, 4);
+        }
+    }
+}
+</script>
+
+<style>
+input{
+    font-weight: bold;
+    color: rgb(29, 113, 134);
+}
+</style>
