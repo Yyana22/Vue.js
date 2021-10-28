b2da7bb1713c367c0ef00b959a3372373d30661e
Yana <sharik.spartak2@mail.ru>
2021-10-28 22:29:53 +0300

    lesson2

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,65 @@
 <template>
     <div>
+
+        <div v-for="(item, idx) in myCollection" :key="`${idx}_parent`"> <!-- при v-for всегда нужен :key -->
+            {{ item }} -- {{ idx }}
+            <!-- <div v-for="(item, idx) in myCollection[idx]" :key="idx">{{ item }} -- {{ idx }}</div> -->
+            </div>
+
+        <div class="logs" v-for="(log, id) in logs" :key="id">{{log}}</div>
+
         <!-- модификаторы у v-model:
         v-model.number - Для автоматического приведения введённого пользователем к Number,
         v-model.lazy - обновляет данные после завершения ввода
         v-model.trim - автоматически обрезает пробелы в начале и в конце строки
         v-model.number.trim - допустимо
         -->
-        <input type="text" v-model.lazy="text1" placeholder="operand0">
         <input type="number" v-model.number="operand1" placeholder="operand1">
+
         <input type="number" 
         v-model.number="operand2" 
         placeholder="operand2" 
         @blur="sendData(operand2)"/>
-        = {{result}}
+
+        <div>= {{result}}</div>
+        <div>= fib {{fibResult}}</div>
+
+        <div class="error" 
+        :class="{'red': error}" 
+        v-if="error">
+        {{error}}  
+        </div> <!--  каждый раз добавляет\удаляет из DOM дерева элемент, происходят все хуки жизненного цикла --> 
+
+        <div class="error" 
+        :class="{'red': error}" 
+        v-show="error">
+        {{error}}  
+        </div> <!--  просто меняет display: none --> 
+
+        <div class="strange-message">
+            <template v-if="result <0"> Получилось отрицательное число </template>
+            <template v-else-if="result < 100"> Получилось число меньше 100 </template>
+            <template v-else>Все остальные числа</template> <!-- v-else не поддерживает условие -->
+        </div>
+
         <div class="keyboard">
-            <button @click="calculate('+')">+</button>
+            <!-- <button @click="calculate('+')">+</button>
             <button @click="calculate('-')">-</button>
             <button @click="calculate('*')">*</button>
             <button @click="calculate('/')">/</button>
             <button @click="calculate('%')">%</button>
-            <button @click="calculate('^')">^</button>
+            <button @click="calculate('^')">^</button> -->
+            <button v-for="operand in operands" 
+                @click="calculate(operand)"
+                :key="operand"
+                :disabled="operand1 === 0"
+                v-bind:title="operand" 
+                :alt="operand" 
+                > <!-- v-bind связывает шаблон html с данными  :alt - при наведении вылезает картинка(?) с operand'ом
+                :disabled, если условие (operand1 === 0) выполняется, уходит возможность взаимодействия с кнопками
+                -->
+                {{operand}}
+            </button>
         </div>
     </div>
 </template>
@@ -29,11 +69,15 @@ export default {
     name: "Calculator",
     data(){
         return{
+            myCollection: [1,2,3,4,5],
+            operands: ['+', '-', '/','*','%','^'],
             text1: '',
             operand1: 0,
             operand2: 0,
+            fibResult: 0,
             result: 0,
             error: '',
+            logs:{},
         }
     },
     //watch позволяет увидеть изменения (новое значение и старое) СМОТРИТ за изменениями
@@ -46,6 +90,7 @@ export default {
     },
     methods: {
         calculate(operation = "+"){
+            this.error = "";
             switch(operation){
                 case '+':
                     this.add();
@@ -68,9 +113,15 @@ export default {
                 default: 
                     break;
             }
+            const key = Date.now() // дата в виде мс обозначается
+            // this.logs[key] = `${this.operand1}${operation}${this.operand2} = ${this.result}`;
+            const value = `${this.operand1}${operation}${this.operand2} = ${this.result}`;
+            // Vue.set(object, propertyName, value)
+            this.$set(this.logs, key, value);
         },
         add(){
             this.result = this.operand1 + this.operand2;
+            this.fibResult = this.fib1 + this.fib2;
         },
         substract(){
             this.result = this.operand1 - this.operand2;
@@ -82,13 +133,13 @@ export default {
 
             const {operand1, operand2 } = this;
             if (operand2 === 0){
-                return this.result = 'Делить на 0 нельзя'
+                return this.error = 'Делить на 0 нельзя'
             }
             this.result = operand1 / operand2;
         },
         divTwo(){
             if(this.operand2 == 0){
-                this.result = 'Делить на 0 нельзя';
+                this.error = 'Делить на 0 нельзя';
             } else {
                 this.result = Math.floor(this.operand1 / this.operand2);
             }
@@ -99,20 +150,33 @@ export default {
         sendData(data){
             console.log('Send Data name', data);
         },
+        fib(n){
+            return n<=1 ? n : this.fib(n-1) + this.fib(n-2);
+        },
     },
-    //ВЫЧИСЛЯЕТ значения после действий пользователя(?)
-    // computed: {
-    //     powWithOperand(){
-    //         return Math.pow(this.operand1, this.operand2);
-    //     },
-    //     powSum(){
-    //         return Math.pow(this.result, 4);
-    //     }
-    // }
+    // ВЫЧИСЛЯЕТ значения после действий пользователя(?)
+    computed: {
+        fib1() {
+            return this.fib(this.operand1)
+        },
+        fib2() {
+            return this.fib(this.operand2)
+        },
+        
+        // powWithOperand(){
+        //     return Math.pow(this.operand1, this.operand2);
+        // },
+        // powSum(){
+        //     return Math.pow(this.result, 4);
+        // }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.red {
+    color: red;
+}
 input{
     font-weight: bold;
     color: rgb(29, 113, 134);
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    {{ date }}
+    {{ dates }}
   </div>
 </template>
 
@@ -10,7 +10,7 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String,
-    date: String,
+    dates: String,
   }
 }
 </script>
